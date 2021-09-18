50c99eb1ee45b8b0dfcf5793b668d235033534cc
martingrzzler <martin.gressler@code.berlin>
2021-09-18 22:47:29 +0200

    added Vec3 class

--- src/math.rs
+++ src/math.rs
@@ -7,6 +7,12 @@ pub struct Vec3 {
 }
 
 #[allow(dead_code)]
+pub type Point3 = Vec3;
+
+#[allow(dead_code)]
+pub type Color = Vec3;
+
+#[allow(dead_code)]
 impl Vec3 {
 	pub fn new() -> Self {
 		Self { e: (0.0, 0.0, 0.0) }
@@ -27,6 +33,14 @@ impl Vec3 {
 	pub fn z(&self) -> f64 {
 		self.e.2
 	}
+
+	pub fn len(&self) -> f64 {
+		self.len_squared().sqrt()
+	}
+
+	pub fn len_squared(&self) -> f64 {
+		self.e.0 * self.e.0 + self.e.1 * self.e.1 + self.e.2 * self.e.2
+	}
 }
 
 impl ops::Neg for Vec3 {
@@ -39,7 +53,7 @@ impl ops::Neg for Vec3 {
 	}
 }
 
-impl ops::AddAssign<Vec3> for Vec3 {
+impl ops::AddAssign for Vec3 {
 	fn add_assign(&mut self, rhs: Vec3) {
 		*self = Self {
 			e: (self.e.0 + rhs.e.0, self.e.1 + rhs.e.1, self.e.2 + rhs.e.2),
@@ -47,7 +61,7 @@ impl ops::AddAssign<Vec3> for Vec3 {
 	}
 }
 
-impl ops::Add<Vec3> for Vec3 {
+impl ops::Add for Vec3 {
 	type Output = Vec3;
 
 	fn add(self, rhs: Vec3) -> Vec3 {
@@ -77,6 +91,42 @@ impl ops::IndexMut<usize> for Vec3 {
 			1 => &mut self.e.1,
 			2 => &mut self.e.2,
 			_ => panic!("index out of bounds"),
+		}
+	}
+}
+
+impl ops::MulAssign for Vec3 {
+	fn mul_assign(&mut self, rhs: Self) {
+		*self = Self {
+			e: (self.e.0 * rhs.e.0, self.e.1 * rhs.e.1, self.e.2 * self.e.2),
+		}
+	}
+}
+
+impl ops::Mul for Vec3 {
+	type Output = Vec3;
+
+	fn mul(self, rhs: Self) -> Self::Output {
+		Self {
+			e: (self.e.0 * rhs.e.0, self.e.1 * rhs.e.1, self.e.2 * self.e.2),
+		}
+	}
+}
+
+impl ops::Div for Vec3 {
+	type Output = Vec3;
+
+	fn div(self, rhs: Vec3) -> Self::Output {
+		Self {
+			e: (self.e.0 / rhs.e.0, self.e.1 / rhs.e.1, self.e.2 / self.e.2),
+		}
+	}
+}
+
+impl ops::DivAssign for Vec3 {
+	fn div_assign(&mut self, rhs: Self) {
+		*self = Self {
+			e: (self.e.0 / rhs.e.0, self.e.1 / rhs.e.1, self.e.2 / self.e.2),
 		}
 	}
 }
@@ -123,5 +173,43 @@ mod test {
 		let mut v = Vec3::from(1.0, 2.0, 3.0);
 		v[0] = 3.3;
 		assert_eq!(v[0], 3.3);
+	}
+
+	#[test]
+	fn test_mul_assign() {
+		let mut v = Vec3::from(1.0, 2.0, 3.0);
+		v *= Vec3::from(1.0, 2.0, 3.0);
+		assert_eq!(v, Vec3::from(1.0, 4.0, 9.0));
+	}
+
+	#[test]
+	fn test_mul() {
+		let res = Vec3::from(1.0, 2.0, 3.0) * Vec3::from(1.0, 2.0, 3.0);
+		assert_eq!(res, Vec3::from(1.0, 4.0, 9.0));
+	}
+
+	#[test]
+	fn test_div() {
+		let res = Vec3::from(1.0, 2.0, 3.0) / Vec3::from(1.0, 2.0, 3.0);
+		assert_eq!(res, Vec3::from(1.0, 1.0, 1.0));
+	}
+
+	#[test]
+	fn test_div_assign() {
+		let mut v = Vec3::from(1.0, 2.0, 3.0);
+		v /= Vec3::from(1.0, 4.0, 3.0);
+		assert_eq!(v, Vec3::from(1.0, 0.5, 1.0));
+	}
+
+	#[test]
+	fn test_len_squared() {
+		let v = Vec3::from(1.0, 2.0, 3.0);
+		assert_eq!(v.len_squared(), 14.0);
+	}
+
+	#[test]
+	fn test_len() {
+		let v = Vec3::from(3.0, 4.0, 0.0);
+		assert_eq!(v.len(), 5.0);
 	}
 }
